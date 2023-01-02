df9a6c4e3b51124afbbb71b1964b0406f3e6c585
Amir Adamov <adamov.aamir@gmail.com>
2023-01-02 10:00:42 +0000

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className="nav">
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/product">Products</NavLink>
+      <NavLink to="/product">Productsss</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/contact">Contact</NavLink>
     </nav>
