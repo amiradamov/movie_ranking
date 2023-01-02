5b4bfc42b2b7daa7e548349cc4ab6434efc5a4ef
Amir Adamov <adamov.aamir@gmail.com>
2023-01-02 12:02:18 +0000

    imdb_top_1000

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,13 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/product">Productsss</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-    </nav>
+    <div id="menu">
+      <nav className="nav">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/product">Movies page</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+    </div>
   );
 }
