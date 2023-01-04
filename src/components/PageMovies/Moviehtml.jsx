e9fd4f4db87c2da0a357297474701c04688e6709
Amir Adamov <adamov.aamir@gmail.com>
2023-01-04 07:12:43 +0000

    Update 10 files

---
+++ src/components/PageMovies/Moviehtml.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import "./Moviehtml.css";
+
+function Moviehtml(props) {
+    return (
+        <ul>
+            <NavLink className="product-item" to={`/product/${props.id}`} >
+                <li>
+                    <div id="parent">
+                        <div id="child1">
+                            <img src={props.img} alt="Poster_Link" />
+                        </div>
+                        <div id="child2">
+                            <h4 className="movie-info">{props.id} <span className="bar"></span>  {props.title} ({props.year})</h4>
+                        </div>
+                        <div id="child3">
+                            <div id="subchild1">
+                                <img src="https://www.pngall.com/wp-content/uploads/9/Golden-Star-PNG-Image-File.png" alt="gold_star" />
+                            </div>
+                            <div id="subchild2">
+                                {props.rating}
+                            </div>
+                        </div>
+                    </div>  
+                </li>
+            </NavLink>
+        </ul>
+    );
+}
+
+export default Moviehtml;
