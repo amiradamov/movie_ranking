407ef4134453615f3eec3ed6b901c3f9e0964ce7
Amir Adamov <adamov.aamir@gmail.com>
2023-01-05 11:03:30 +0000

    Update ProductInfo.jsx

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router";
+import movies from "../imdb_top_1000";
 
 export default function ProductInfo() {
   const { productId } = useParams();
 
-  return <div>This is the info page for product {productId}</div>;
+  return <div>This is the info page for product {movies.id}</div>;
 }
